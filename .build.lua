de4fe88c7e68c58b1e1c9dceb0f5f0577ae239c3
SquidDev <bonzoweb@hotmail.co.uk>
2016-10-29 22:50:40 +0100

    Add experimental build system
    This is going to fail as the Gist options
    don't exist

---
+++ .build.lua
@@ -0,0 +1,13 @@
+local func, msg = loadfile("bootstrap.lua", _ENV)
+if not func then
+	howlci.status("fail", "Cannot load boostrapper: " .. (msg or "<no msg>"))
+	return
+end
+
+local ok, msg = pcall(func, "-v")
+if not ok then
+	howlci.status("fail", "Failed running task: " .. (msg or "<no msg>"))
+	return
+end
+
+howlci.status("ok", "Everything built correctly!")
