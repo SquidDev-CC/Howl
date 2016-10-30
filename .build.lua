b7850f0884d87e82621e0b9234a2cdb626918bd8
SquidDev <bonzoweb@hotmail.co.uk>
2016-10-30 23:44:43 +0000

    Ensure howl.ci build passes

--- .build.lua
+++ .build.lua
@@ -1,10 +1,14 @@
+local handle = fs.open(".howl/settings.lua", "w")
+handle.write('{githubKey="not-set"}')
+handle.close()
+
 local func, msg = loadfile("bootstrap.lua", _ENV)
 if not func then
 	howlci.status("fail", "Cannot load boostrapper: " .. (msg or "<no msg>"))
 	return
 end
 
-local ok, msg = pcall(func, "-v")
+local ok, msg = pcall(func, "-v", "build")
 if not ok then
 	howlci.status("fail", "Failed running task: " .. (msg or "<no msg>"))
 else
