995b29ab2b2527aba5ed5e27ff2af75feaadf41e
MineRobber___T <MineRobber9000@users.noreply.github.com>
2017-08-18 17:36:18 -0400

    Update init.lua

--- howl/platform/init.lua
+++ howl/platform/init.lua
@@ -4,5 +4,9 @@
 if fs and term then
 	return require "howl.platform.cc"
 else
-	return require "howl.platform.native"
+	if require("component") and pcall(function() return require("component").computer end) then
+		return require "howl.platform.oc"
+	else
+		return require "howl.platform.native"
+	end
 end
